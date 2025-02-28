d045562bd4ef837345a8ecb8afcba9942fb6a0c8
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-02-28 20:05:58 +0100

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Jakub Malinkiewicz on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
